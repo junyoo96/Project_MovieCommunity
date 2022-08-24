d6b69e6c2e2435b6cd74fc5c57e3e53f3ca5e0e4
root <root@DESKTOP-9JSPC7N.localdomain>
2022-08-24 15:32:58 +0900

    Set project environment

---
+++ src/main/java/jun/moviecommunity/MoviecommunityApplication.java
@@ -0,0 +1,13 @@
+package jun.moviecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jun/moviecommunity/MoviecommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package jun.moviecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
