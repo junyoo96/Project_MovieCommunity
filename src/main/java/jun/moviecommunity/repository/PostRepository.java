9706fa4ed4e9c76004169c60f0b27a05eaf5fa5c
Youngjun Yoo <junyoo96@gmail.com>
2022-10-15 17:47:36 +0900

    게시물 검색 Spring Data JPA 스타일로 변경

--- src/main/java/jun/moviecommunity/repository/PostRepository.java
+++ src/main/java/jun/moviecommunity/repository/PostRepository.java
@@ -2,42 +2,29 @@ package jun.moviecommunity.repository;
 
 import jun.moviecommunity.domain.Post;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class PostRepository {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
-    private final EntityManager em;
+    /**
+     * 사용자 id로 게시물 조회
+    **/
+    @Query("select p from Post p where p.author.id = :userId")
+    List<Post> findAllByUserId(Long userId);
 
-    public void save(Post post) {
-        em.persist(post);
-    }
-
-    public Post findOne(Long id) {
-        return em.find(Post.class, id);
-    }
-
-    public List<Post> findAll() {
-        return em.createQuery("select p from Post p", Post.class).getResultList();
-    }
-
-    public List<Post> findAllByUserId(Long userId) {
-        return em.createQuery("select p from Post p where p.author.id = :userId", Post.class)
-                .setParameter("userId", userId)
-                .getResultList();
-    }
-
-    public void delete(Long id) {
-        em.remove(findOne(id));
-    }
-
-    public List<Post> searchByTitle(String keyword) {
-        return em.createQuery("select p from Post p where p.title like :keyword")
-                .setParameter("keyword", "%" + keyword + "%")
-                .getResultList();
-    }
+    /**
+     * 게시물 제목 또는 내용으로 게시물 검색
+    **/
+    @Query(
+        value = "SELECT p FROM Post p WHERE p.title LIKE %:keyword% OR p.content LIKE %:keyword%",
+        countQuery = "SELECT COUNT(p.id) FROM Post p WHERE p.title LIKE %:keyword% OR p.content LIKE %:keyword%"
+    )
+    Page<Post> findAllByTitleOrContent(@Param("keyword") String keyword, Pageable pageable);
 }
-
